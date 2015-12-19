f254c7ec25dba84ff4ac80a1b8c7cfaee83bfa4d
muurastr <muura@mail.ru>
2015-12-19 17:43:46 +0300

    Add Сlienе entity

--- Gemfile
+++ Gemfile
@@ -6,6 +6,6 @@ gem "activerecord"
 gem "sinatra-activerecord"
 gem "sinatra-contrib"
 
-group :development
+group :development do
 	gem "tux"
 end
--- app.rb
+++ app.rb
@@ -2,6 +2,13 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+#подключение к БД
+set :database, "sqlite3:barbershop.db"
+#создать класс, кот будет представлять сущность
+#что-то, что описывает (клиент, barber...)
+class Client < ActiveRecord::Base
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
