3632842360312045a853ec9302593ab44d5164a9
muurastr <muura@mail.ru>
2015-12-23 14:02:24 +0300

    Add Clients table migration

---
+++ db/migrate/20151223104711_create_clients.rb
@@ -0,0 +1,15 @@
+class CreateClients < ActiveRecord::Migration
+  def change
+  	#метод create_table принимает параметр в виде символа с именем табицы (здесь client)
+  	create_table :client do |t|
+  		#в таблице client буду созданы столбцы name, phone... типа text
+  		t.text :name
+  		t.text :phone
+  		t.text :datestamp
+  		t.text :barber
+  		t.text :color
+#метод - будут созданы 2 доп столбца: created_at & updarted_at
+  		t.timestamps 
+  	end
+  end
+end
